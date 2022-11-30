9733bf1a1711eebce066beb551220027c47d59ad
Kavin Bharathi <66783110+kavinbharathii@users.noreply.github.com>
2022-11-30 13:53:56 +0530

    added -web , -nn, -cv

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::process;
 
+// ====================================================== Webdev ====================================================== //
+
 fn webdev(project_name: &str) {
     if !Path::new(project_name).exists() {
-        let _dire = fs::create_dir(project_name).expect("Error encountered while creating file!");
+        let _dire = fs::create_dir(project_name).expect(format!("Error encountered while creating project: {}!", project_name).as_str());
     } else {
-        println!("This project already exists");
+        println!("{} already exists", project_name);
         process::exit(0);
     }
 
@@ -78,12 +80,103 @@ const dev = document.getElementById('dev')
 "#, project_name).as_bytes()).expect("Error while writing to app.js");
 }
 
+// ====================================================== Neuraldev ====================================================== //
+
+fn neuraldev(project_name: &str) {
+    if !Path::new(project_name).exists() {
+        let _dire = fs::create_dir(project_name).expect(format!("Error encountered while creating project: {}!", project_name).as_str());
+    } else {
+        println!("{} already exists", project_name);
+        process::exit(0);
+    }
+
+    let mut py = File::create(format!("{}/{}", project_name, "main.py")).expect("Failed to create main.py");
+    py.write_all(r##"
+
+# Tensorlflow imports
+import tensorflow as tf
+from tensorflow import keras
+
+# Utitliy imports
+import json
+import numpy as np
+import pandas as pd
+
+model = tf.keras.Sequential([
+    # Your layers here...
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+"##.as_bytes()).expect("Error while writing to main.py");
+
+    let json = File::create(format!("{}/{}", project_name, "data.json")).expect("Failed to create data.json");
+
+}
+
+// ====================================================== cv-dev ====================================================== //
+
+fn cvdev(project_name: &str) {
+    if !Path::new(project_name).exists() {
+        let _dire = fs::create_dir(project_name).expect(format!("Error encountered while creating project: {}!", project_name).as_str());
+    } else {
+        println!("{} already exists", project_name);
+        process::exit(0);
+    }
+
+    let mut py = File::create(format!("{}/{}", project_name, "main.py")).expect("Failed to create main.py");
+    py.write_all(r##"
+
+# Required libraries
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+video = cv2.VideoCapture(0)
+
+# Video loop
+while True:
+    ret, frame = video.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    im_pil = Image.fromarray(img)
+    im_np = np.asarray(im_pil)
+    image = ImageOps.fit(im_pil, size)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+"##.as_bytes()).expect("Error while writing to main.py");
+}
+
 fn main() {
+
     match std::env::args().nth(1) {
         Some(project_name) => {
-            webdev(&project_name);
-            println!("{} was created successfully!", project_name);
+            match std::env::args().nth(2) {
+                Some(project_mode) => {
+                    match project_mode.as_str() {
+                        "-web" => webdev(&project_name),
+                        "-nn" => neuraldev(&project_name),
+                        "-cv" => cvdev(&project_name),
+                        _ => {
+                            println!("Provide a proper flag: [-web] [-nn] [-cv]");
+                            process::exit(0)
+                        }
+                    }
+                },
+                None => {
+                    println!("No project type given: Include [-web] [-nn] [-cv]");
+                    process::exit(0);
+                }
+            }
         },
+
         None => {
             println!("No project name given");
             process::exit(0);
